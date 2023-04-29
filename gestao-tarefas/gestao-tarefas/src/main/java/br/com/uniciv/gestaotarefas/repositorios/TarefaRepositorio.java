ca1c14c624099a8b856af92aeb8241ed961ff527
Junior Martins <juniorsoaresmartins@gmail.com>
2023-04-29 11:40:47 -0400

    FEAT - variedades de soluções do Spring Data Jpa.

--- gestao-tarefas/gestao-tarefas/src/main/java/br/com/uniciv/gestaotarefas/repositorios/TarefaRepositorio.java
+++ gestao-tarefas/gestao-tarefas/src/main/java/br/com/uniciv/gestaotarefas/repositorios/TarefaRepositorio.java
@@ -1,7 +1,31 @@
 package br.com.uniciv.gestaotarefas.repositorios;
 
 import br.com.uniciv.gestaotarefas.modelos.Tarefa;
+import br.com.uniciv.gestaotarefas.modelos.TarefaCategoria;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
-public interface TarefaRepositorio extends JpaRepository<Tarefa, Integer> { }
+import java.util.List;
+
+public interface TarefaRepositorio extends JpaRepository<Tarefa, Integer> {
+
+  // ---------- Derived Queries ---------- //
+  List<Tarefa> findByDescricao(String descricao);
+
+  List<Tarefa> findByDescricaoLike(String descricao);
+
+
+  // ---------- JPQL ---------- //
+  @Query("select t from Tarefa t inner join t.tarefaCategoria c where c.nome = ?1")
+  List<Tarefa> findByNomeCategoria(String nomeCategoria);
+
+
+  // ---------- Native Query ---------- //
+  @Query(value = "select t.* from tarefas t inner join tarefas_categoria tc on t.tarefa_categoria_id = tc.id where tc.nome = ?1", nativeQuery = true)
+  List<Tarefa> findByNomeDeCategoria(String nomeDeCategoria);
+
+
+  // ---------- Named Query (anotação na classe) ---------- //
+  List<Tarefa> tarefasPorCategoria(String tarefaCategoria);
+}
 
