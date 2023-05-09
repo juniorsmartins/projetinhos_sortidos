87598adb859ecf1b05b57063bd3115eeeae4b186
Junior Martins <juniorsoaresmartins@gmail.com>
2023-05-09 18:54:00 -0400

    FEAT - add endpoint TarefaCategoria.

--- gestao-tarefas/gestao-tarefas/src/main/java/br/com/uniciv/gestaotarefas/controladores/TarefaCategoriaControlador.java
+++ gestao-tarefas/gestao-tarefas/src/main/java/br/com/uniciv/gestaotarefas/controladores/TarefaCategoriaControlador.java
@@ -1,17 +1,20 @@
 package br.com.uniciv.gestaotarefas.controladores;
 
+import br.com.uniciv.gestaotarefas.controladores.dtos.request.TarefaCategoriaResumo;
 import br.com.uniciv.gestaotarefas.controladores.dtos.response.TarefaCategoriaResponse;
 import br.com.uniciv.gestaotarefas.modelos.TarefaCategoria;
 import br.com.uniciv.gestaotarefas.servicos.TarefaCategoriaService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(path = {"/categorias"})
@@ -37,6 +40,15 @@ public class TarefaCategoriaControlador {
       .map(this.tarefaCategoriaService.consultarPorId(id), TarefaCategoriaResponse.class);
   }
 
+  @PostMapping
+  public TarefaCategoriaResponse cadastrar(@RequestBody TarefaCategoriaResumo tarefaCategoriaResumo) {
+    TarefaCategoria categoria = this.mapper.map(tarefaCategoriaResumo, TarefaCategoria.class);
+    return this.mapper.map(this.tarefaCategoriaService.salvar(categoria), TarefaCategoriaResponse.class);
+  }
 
+  @DeleteMapping(path = "/{id}")
+  public void excluir(@PathVariable Integer id) {
+    this.tarefaCategoriaService.deletarPorId(id);
+  }
 }
 
