a5c9c4b10b9bb37b3a67e29a88ca77d14f3af8f1
Junior Martins <juniorsoaresmartins@gmail.com>
2023-06-04 13:44:49 -0400

    FEAT: configuração de WebSecurityConfig.

--- gestao-tarefas/gestao-tarefas/src/main/java/br/com/uniciv/gestaotarefas/security/config/WebSecurityConfig.java
+++ gestao-tarefas/gestao-tarefas/src/main/java/br/com/uniciv/gestaotarefas/security/config/WebSecurityConfig.java
@@ -2,45 +2,45 @@ package br.com.uniciv.gestaotarefas.security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
-import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfig {
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-  // O SecurityFilterChain define quais caminhos de URL devem ser protegidos e quais não devem.
-  @Bean
-  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+  /**
+   *  AUTENTICAÇÃO
+   */
+  @Override
+  protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    super.configure(auth);
+  }
 
-    httpSecurity
-      .authorizeHttpRequests((requests) -> requests
-        .requestMatchers("/", "/home").permitAll()
-        .anyRequest().authenticated())
-      .formLogin((form) -> form
-        .loginPage("/login")
-        .permitAll())
-      .logout((logout) -> logout
-        .permitAll());
-
-    return httpSecurity.build();
+  /**
+   *  AUTORIZAÇÃO
+   */
+  @Override
+  protected void configure(HttpSecurity http) throws Exception {
+    super.configure(http);
   }
 
   @Bean
-  public UserDetailsService userDetailsService() {
+  @Override
+  public AuthenticationManager authenticationManagerBean() throws Exception {
+    return super.authenticationManagerBean();
+  }
 
-    UserDetails user = User.withDefaultPasswordEncoder()
-      .username("user")
-      .password("123")
-      .roles("USER")
-      .build();
-
-    return new InMemoryUserDetailsManager(user);
+  @Bean
+  public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
   }
 }
 
